5792cfb4413e76d987104d6c69aa74deb014d442
Martin Marguerat <49449909+margueratm@users.noreply.github.com>
2020-10-21 12:56:56 +0200

    Added multiple entry options

--- ArchiveScanTool/process.cs
+++ ArchiveScanTool/process.cs
@@ -12,7 +12,6 @@ namespace ArchiveScanTool
 {
     class process
     {
-
         public void ProcessFile()
         {
             string path = Directory.GetCurrentDirectory();
@@ -21,9 +20,10 @@ namespace ArchiveScanTool
             {
                 //Extract name
                 string fileName = file.Substring(path.Length + 1);
-                if (fileName.Length == 15)
+                int fileNameLength = fileName.Length;
+                string[] folder = ExtractName(fileName, fileNameLength);
+                if (folder[0] != "00" && folder[1] != "0000" && folder[2] != "000")
                 {
-                    string[] folder = ExtractName(fileName);
                     string newFileName = folder[0] + "." + folder[1] + "." + folder[2] + "-scan.pdf";
                     //string newPath = "\\TERMINAL\\Data\\Chantiers\\" + folder[0] + "\\" + folder[1] + "\\" + folder[2] + "\\" + newFileName;
                     string newPath = path + "\\ok\\" + newFileName;
@@ -47,14 +47,29 @@ namespace ArchiveScanTool
             }
         }
 
-        private string[] ExtractName(string file)
+        private string[] ExtractName(string file, int fileNameLength)
         {
             string[] folder = new string[3];
 
-            folder[0] = file.Substring(0, 2);
-            folder[1] = file.Substring(3, 4);
-            folder[2] = file.Substring(8, 3);
-
+            switch (fileNameLength)
+            {
+                case 15:
+                    folder[0] = file.Substring(0, 2);
+                    folder[1] = file.Substring(3, 4);
+                    folder[2] = file.Substring(8, 3);
+                    break;
+                case 12:
+                    folder[0] = file.Substring(0, 2);
+                    folder[1] = file.Substring(2, 4);
+                    folder[2] = file.Substring(6, 3);
+                    break;
+                default:
+                    folder[0] = "00";
+                    folder[1] = "0000";
+                    folder[2] = "000";
+                    break;
+            }
+            folder[0] = folder[0].ToUpper();
             return folder;
         }
     }
