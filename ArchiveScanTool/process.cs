d0287a679d8bb6648607e8a57e096a897a8f9a2b
Martin Marguerat <49449909+margueratm@users.noreply.github.com>
2020-10-21 09:59:59 +0200

    Ajoutez des fichiers projet.

---
+++ ArchiveScanTool/process.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveScanTool
+{
+    class process
+    {
+        string path;
+        string fileName;
+        string folder;
+    }
+}
